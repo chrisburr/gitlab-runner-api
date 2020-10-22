dd0fc9239a14d56e9a78399c1a6f7d2e3e4b219f
Chris Burr <christopher.burr@cern.ch>
2020-10-22 18:37:48 +0200

    Fix PyPI license metadata

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT",
+        "License :: OSI Approved :: MIT License",
     ],
     python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
 )
